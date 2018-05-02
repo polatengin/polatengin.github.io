615886d42c0ea2ce17d022eb576fabdaae4ef5f1
Engin Polat <polatengin@outlook.com>
2018-05-02 15:45:18 +0300

    pwa, offline calisabilme icin serviceworker eklendi

--- sw.js
+++ sw.js
@@ -1,52 +1,44 @@
-/*
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches
-      .open('enginpolatcom-v1')
-      .then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/manifest.json',
-          '/assets/bundle.min.css?v1',
-          '/assets/images.png'
-        ]);
-      })
-  );
+self.addEventListener('install', function (event) {
+  event.waitUntil(function () {
+    return caches.open('enginpolat-offline').then(function (cache) {
+      return cache.addAll(['/offline.html', '/index.html']);
+    });
+  });
 });
 
-self.addEventListener("fetch", function(event) {
-  if (event.request.method !== 'GET') {
-    return;
-  }
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(function(cached) {
-        var networked = fetch(event.request)
-          .then(fetchedFromNetwork, unableToResolve)
-          .catch(unableToResolve);
-        return cached || networked;
-
-        function fetchedFromNetwork(response) {
-          var cacheCopy = response.clone();
-          caches
-            .open('enginpolatcom-pages-v1')
-            .then(function add(cache) {
-              cache.put(event.request, cacheCopy);
-            });
-          return response;
-        }
-
-        function unableToResolve () {
-          return new Response('<h1>Service Unavailable</h1>', {
-            status: 503,
-            statusText: 'Service Unavailable',
-            headers: new Headers({
-              'Content-Type': 'text/html'
-            })
-          });
-        }
-      })
-  );
+self.addEventListener('fetch', function (event) {
+  event.respondWith(checkResponse(event.request).catch(function () { return returnFromCache(event.request) }));
+  event.waitUntil(addToCache(event.request));
 });
-*/
+
+var checkResponse = function (request) {
+  return new Promise(function (fulfill, reject) {
+    fetch(request).then(function (response) {
+      if (response.status !== 404) {
+        fulfill(response)
+      } else {
+        reject()
+      }
+    }, reject)
+  });
+};
+
+var addToCache = function (request) {
+  return caches.open('enginpolat-offline').then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  });
+};
+
+var returnFromCache = function (request) {
+  return caches.open('enginpolat-offline').then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status == 404) {
+        return cache.match('offline.html')
+      } else {
+        return matching
+      }
+    });
+  });
+};
